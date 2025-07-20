5fbc29cfa31247d2fa6b9611ba1a74486a02b0bf
flowcodetechnology <support@flowcode.co.ke>
2025-07-20 08:09:25 +0300

    new config

--- config.php
+++ config.php
@@ -1,8 +1,11 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 /* Configuration of the site */
-define('DATABASE_SERVER',   '');
-define('DATABASE_USERNAME', '');
-define('DATABASE_PASSWORD', '');
-define('DATABASE_NAME',     '');
-define('SITE_URL',          '');
+define('DATABASE_SERVER',   'localhost');
+define('DATABASE_USERNAME', 'gponzghq_posuser');
+define('DATABASE_PASSWORD', 'Sinister101@1738');
+define('DATABASE_NAME',     'gponzghq_seamlesdemo');
+define('SITE_URL',          'https://mailer.flowcode.co.ke/');
+?>
